310f50643eae3348de1f1c3c2bc84483b658dbf6
bharat619 <bharat.h@kiprosh.com>
2020-02-17 14:10:39 +0530

    adding components people-list, adapters and mirage

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,9 +1,10 @@
 import Component from "@ember/component";
 
 export default Component.extend({
+  isWide: false,
   actions: {
-    showPerson(person) {
-      alert(person);
+    toggleImageSize() {
+      this.toggleProperty("isWide");
     }
   }
 });
